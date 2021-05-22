71445821052b63d3451c7ee6364105fe74df9de3
Bret Fernberg <bret.fernberg@gmail.com>
2021-05-21 23:23:27 -0600

    beginning skeleton

--- db/seed.sql
+++ db/seed.sql
@@ -5,3 +5,25 @@ CREATE TABLE bbox_users(
     user_name VARCHAR(12)
 );
 
+CREATE TABLE bbox_games(
+    game_id SERIAL PRIMARY KEY,
+    game_name VARCHAR(100),
+    game_description VARCHAR(1500),
+    game_total_plays int
+);
+
+CREATE TABLE bbox_stats(
+    stat_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES bbox_users(user_id),
+    game_id INT REFERENCES bbox_games(game_id),
+    times_played int,
+    total_wins int,
+    high_score int
+);
+
+CREATE TABLE bbox_prompts(
+    prompt_id SERIAL PRIMARY KEY,
+    game_id INT REFERENCES bbox_games(game_id),
+    prompt VARCHAR(280),
+    prompt_type VARCHAR(40)
+);
